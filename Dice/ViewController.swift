8b279455be67e3d1cdfccb68a85a8ecf837a065d
Zahid001 <islam35-1483@diu.edu.bd>
2019-02-10 01:44:51 +0600

    rolling completed

--- Dice/ViewController.swift
+++ Dice/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var dice1:Int = 0
+    var dice2:Int = 0
+    
+    @IBOutlet weak var image1: UIImageView!
+    
+    @IBOutlet weak var image2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func rolling(_ sender: Any) {
+        
+        dice1 = Int.random(in: 1...6)
+        dice2 = Int.random(in: 1...6)
+        
+        image1.image = UIImage(named: "dice\(dice1)" )
+        image2.image = UIImage(named: "dice\(dice2)" )
+        
+    }
+    
 }
 
